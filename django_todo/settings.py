2a205541852c452a3b94a06da89a9fb9bef2c583
BXS-Archipelago <bxs@tutanota.com>
2021-06-24 16:40:05 +0000

    strip-back allowed_hosts, etc to do runserver

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y0%nx*s72p+wol@t(^t97s!rf3#r)1516xymximgdfek$^_vxa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bxs-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
